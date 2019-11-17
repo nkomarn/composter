00d342b6d3ee708cfb36dd18e8977144c2f7874d
Mykyta <nkomarn@hotmail.com>
2019-11-16 18:57:36 -0800

    ✨ Clean up pom n' stuff

--- src/main/java/xyz/nkomarn/world/World.java
+++ src/main/java/xyz/nkomarn/world/World.java
@@ -1,17 +1,7 @@
 package xyz.nkomarn.world;
 
-import xyz.nkomarn.model.Chunk;
-import xyz.nkomarn.model.Location;
-import xyz.nkomarn.nbt.*;
-import xyz.nkomarn.world.region.RegionFile;
-
-import java.io.DataOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class World {
+
 
 
 }
