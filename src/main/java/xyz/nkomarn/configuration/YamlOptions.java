d7fbc1b4d690769e3cb5be2b95586c56026624c9
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2019-11-22 00:13:08 -0700

    ⚙️ Configuration

--- src/main/java/xyz/nkomarn/configuration/YamlOptions.java
+++ src/main/java/xyz/nkomarn/configuration/YamlOptions.java
@@ -1,6 +1,5 @@
 package xyz.nkomarn.configuration;
 
-import com.sun.istack.internal.NotNull;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileReader;
@@ -10,65 +9,97 @@ import java.util.Arrays;
 import java.util.Map;
 
 public class YamlOptions {
-    public String[] ntw;
-    public String[] msg;
-    public String[] mtd;
 
-    /* The three methods get the YAML values */
-    public void network(){
-        String net;
-        net = yaml("network");
-        ntw = makeArray(net);
-    }
-    public void messages(){
-        String message;
-        message = yaml("message");
-        msg = makeArray(message);
-    }
 
-    public void motd(){
-        String mot;
-        mot = yaml("motd");
-        mtd = makeArray(mot);
+    public static void main(String[] args){
+        String get = yamlResult("motd.enabled");
+        System.out.println(Boolean.parseBoolean(get));
     }
 
 
-    /*Just simply turns a string into a String array. */
+    public String getString(String arg) {
+        return yamlResult(arg);
+    }
+
+    public Boolean getBoolean(String arg){
+        String get = yamlResult(arg);
+        return Boolean.parseBoolean(get);
+    }
+
+    public int getInt(String arg){
+        String get = yamlResult(arg);
+        return Integer.parseInt(get);
+    }
+
+    public Double getDouble(String arg){
+        String get = yamlResult(arg);
+        return Double.parseDouble(get);
+    }
+
+    public Float getFloat(String arg){
+        String get = yamlResult(arg);
+        return Float.parseFloat(get);
+    }
+
+    public ArrayList<String> getList(String arg){
+        ArrayList<String> ar = new ArrayList<String>();
+        ar.add(arg);
+        return ar;
+    }
+
+
+
+    private static String yamlResult(String arg){
+
+        String parse = stringSplit(arg, 0);
+        String yaml = yaml(parse);
+        String[] children = makeArray(yaml);
+
+        String result = "";
+        for(int i=0; i<children.length; i++){
+            if(children[i].contains(stringSplit(arg, 1))){
+                result = children[i].substring(children[i].indexOf("=") + 1);
+            }
+        }
+        return result;
+    }
+
+    private static String stringSplit(String arg, int index){
+        String[] split = arg.split("\\.");
+        return split[index];
+    }
+
     private static String[] makeArray(String arg) {
         String[] arr = arg.split(",");
         ArrayList<String> a = new ArrayList<String>();
 
         for (int i = 0; i < arr.length; i++) {
-
             String yaml;
-            yaml = arr[i].substring(arr[i].indexOf("=") + 1);
+            yaml = arr[i].substring(arr[i].indexOf("{") + 1);
             a.add(yaml.split("}")[0]);
-
         }
+
         String[] ar = new String[a.size()];
         ar = a.toArray(ar);
         return ar;
     }
 
-
-    /* Gets the YAML */
-    @NotNull
-    private static String yaml(String yamlArgument){
+    private static String yaml(String yamlArgument) {
         final String filePath = "src/main/resources/composter.yml";
         ArrayList<String> value = new ArrayList<String>();
         Yaml yaml = new Yaml();
 
         try {
-
             Reader yamlFile = new FileReader(filePath);
             Map<String, Object> yamlMaps = yaml.load(yamlFile);
             value.add(yamlMaps.get(yamlArgument).toString());
-
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+
         return " " + value;
     }
 
 }
+
